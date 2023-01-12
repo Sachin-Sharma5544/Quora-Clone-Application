e8d6ea75c4f7c0dffc8a18595975a40107b10d55
Sachin Sharma <sachin.kumarsharam09@gmail.com>
2023-01-12 20:21:10 +0530

    Welcome page done, Articles page is in progress

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 const path = require("path");
 
+const hbs = require("hbs");
 const express = require("express");
 const app = express();
 
 //Important variables declarations
-const PORT = 5000;
+const PORT = 5001;
 const dirname = __dirname;
 const staticFolder = "static";
 
 const welcomePage = "welcomePage.hbs";
+const articlesPage = "articlesPage.hbs";
 const viewsFolder = "views";
 
 app.use(express.static(path.join(dirname, staticFolder)));
@@ -16,9 +18,17 @@ app.use(express.static(path.join(dirname, staticFolder)));
 //Setting view engine
 app.set("view engine", "hbs");
 
+hbs.registerPartials(__dirname + "/views/partials");
+
 app.get("/", (req, res) => {
     res.render(path.join(dirname, viewsFolder, welcomePage), {
-        title: "Quora",
+        title: "Jawaab Paao",
+    });
+});
+
+app.get("/articles", (req, res) => {
+    res.render(path.join(dirname, viewsFolder, articlesPage), {
+        title: "Jawaab Paao ",
     });
 });
 
