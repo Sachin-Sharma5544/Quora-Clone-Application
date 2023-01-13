4d8ece5df3bf6bc38db69e84a77b6c7072485f64
Sachin Sharma <sachin.kumarsharam09@gmail.com>
2023-01-13 17:18:36 +0530

    Showing single article page done

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const welcomePage = "welcomePage.hbs";
 const articlesPage = "articlesPage.hbs";
 const viewsFolder = "views";
 
-let num = 3;
+let num;
 
 app.use(express.static(path.join(dirname, staticFolder)));
 app.use(express.urlencoded({ extended: true }));
@@ -39,6 +39,8 @@ const articles = [
         answer: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt harum eum repudiandae, nobis rerum laboriosam rem vitae corporis maxime atque porro error voluptatum dolorem quam ducimus culpa aliquam at. Iste debitis dicta consequuntur quos, dolorem consequatur, explicabo minus excepturi eveniet optio repellendus. Explicabo reprehenderit accusantium eius. Quasi nihil quas a doloribus soluta fuga dicta impedit fugiat! Consectetur esse aliquam dolores at laboriosam iusto vel ipsa dolorem, optio modi aperiam! Distinctio nobis hic natus atque ea ipsam. Asperiores nihil ducimus ipsum praesentium aut quaerat atque id at vel accusantium dolores, nobis, rem necessitatibus, quo sunt quae impedit animi est nulla laboriosam. Quod fuga cum veritatis itaque voluptatibus odio magnam perferendis fugit quam adipisci dignissimos ex quo, sint natus dolorem impedit a ullam optio pariatur voluptate quasi ipsum debitis, distinctio doloremque. Quo quis eius non. Rem impedit totam quidem veniam qui esse iusto provident est obcaecati ratione. Error voluptatum aliquam alias facilis.",
     },
 ];
+
+num = articles.length + 1;
 
 app.get("/", (req, res) => {
     res.render(path.join(dirname, viewsFolder, welcomePage), {
@@ -71,6 +73,19 @@ app.post("/articles", (req, res) => {
 
 app.get("/article/new", (req, res) => {
     res.render("createArticle");
+});
+
+app.get("/articles/:id", (req, res) => {
+    const { id } = req.params;
+    const myFilteredArt = articles.filter(
+        (article) => article.id === parseInt(id)
+    );
+    console.log(myFilteredArt);
+
+    res.render("showArticle", {
+        articles: myFilteredArt,
+        hasArticles: myFilteredArt.length > 0,
+    });
 });
 
 app.listen(PORT, "localhost", () => {
